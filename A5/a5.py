eba80a937f8d82331ba8e2f5fd5fb4c7584e6029
Jonte Puschmann <106771120+Skienex@users.noreply.github.com>
2023-10-05 10:35:16 +0200

    finished a5

--- A5/a5.py
+++ A5/a5.py
@@ -1,18 +1,27 @@
-with open("tour.txt", "r") as file:
-    lines = file.readlines()
+def open_tour(num):
+    with open(f"tour{num}.txt", "r") as file:
+        lines = file.readlines()
+    return lines
 
-locations = []
-for line in lines[1:]:
-    location, year, essential, distance = line.strip().split(',')
-    locations.append({
-        "name": location,
-        "year": int(year),
-        "essential": essential == 'X',
-        "distance": int(distance)
-    })
+def calculate(lines):
+    locations = []
+    for line in lines[1:]:
+        location, year, essential, distance = line.strip().split(',')
+        locations.append({
+            "name": location,
+            "year": int(year),
+            "essential": essential == 'X',
+            "distance": int(distance)
+        })
 
-essential_locations = [loc for loc in locations if loc["essential"]]
-essential_locations.sort(key=lambda loc: loc["year"])
+    essential_locations = [loc for loc in locations if loc["essential"]]
+    essential_locations.sort(key=lambda loc: loc["year"])
 
-for loc in essential_locations:
-    print(f"{loc['name']} ({loc['year']})")
+    return essential_locations
+
+for i in range(1, 6):
+    lines = open_tour(i)
+    essential_locations = calculate(lines)
+    print(f"\n\ntour{i}.txt:")
+    for loc in essential_locations:
+        print(f"{loc['name']} ({loc['year']})")
